b4dd5313fa7fb1e9887534a01a9e352d07e3dd61
crisHermA02 <crishermosilla02@gmail.com>
2026-09-25 01:24:46 +0000

    Se instaló y configuró React Bootstrap

--- src/components/atoms/boton-enviar.jsx
+++ src/components/atoms/boton-enviar.jsx
@@ -1,7 +1,7 @@
 function boton(props) {
   const variante = props.variante || "primary";
   return (
-    <button className={`btn btn-${variante}`} onClick={props.onClick}>
+    <button className={`btn btn-${variante}`} onClick={props.onClick} >
       {props.texto}
     </button>
   );
